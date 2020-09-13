851e5111db5b7276c9f23f6cdd71d7962cbbb899
Rysc0 <boris.bastek@gmail.com>
2020-09-13 11:52:04 +0200

    New function, new way of instering, formatting

--- linked_list.cpp
+++ linked_list.cpp
@@ -2,7 +2,7 @@
 #include <ctime>
 using namespace std;
  
- 
+// linked list structure
 struct node{
     int value;
     int index;
@@ -22,12 +22,18 @@ list *add_to_list(int value, int index){
     return result;
 }
 
+// insert at head
+list *insert_at_head(list *head, int value, int index){
+    list *temp = add_to_list(value, index);
+    temp->next = head;
+    head = temp; 
+    return head;
+}
 
 // find tail of the list
 list *findEnd(list *head){
     list *hmm = head;
     while(hmm != NULL){
-
         if(hmm->next == NULL){
             cout << "Last node has value of: " << hmm->value << " and index of: " << hmm->index << endl;
             return hmm;
@@ -36,6 +42,7 @@ list *findEnd(list *head){
             hmm = hmm->next;
         }
     }
+    return 0; //warning: control reaches end of non-void function
 }
 
 // find element of the list
@@ -43,19 +50,18 @@ list *findNode(list* head, int index){
     list *temporary = head;
     while(temporary != NULL){
         if(temporary->index == index){
-            cout << "Node with index " << temporary->index << " found!" << endl;
-            cout << "Value of the node with index " << temporary->index << " is " << temporary->value << endl;
+            cout << "Node with index " << temporary->index << " found! It's value is: " << temporary->value << endl;
             return temporary;
         }
         else{
             temporary = temporary->next;
         }
     }
+    return 0; // warning: control reaches end of non-void function
 }
 
 // add new node at the end of list
-void insert_at_the_end(int value, int index, list *head){
-    list *tail = findEnd(head);
+void insert_at_the_end(int value, int index, list *tail){
     list *ptr = add_to_list(value,index);
     ptr->next = tail->next;
     tail->next = ptr;
@@ -63,15 +69,10 @@ void insert_at_the_end(int value, int index, list *head){
 
 // add new node in between two nodes
 void insert_inbetween(int value, int index, int index2, list *head){
-    //   insert_inbetween(15,5,6,head);
     list *ins = findNode(head,index2);
-
     list *temp = add_to_list(15,index);
-
     temp->next = ins->next;
     ins->next = temp;
-     
-
 }
 
 // dealocate and delete linked list/free up memory
@@ -83,49 +84,63 @@ void destroy_list(list* head){
         delete help;      
         i++;  
     }
-    
     delete head;
     cout << "deleted " << i << " nodes" << endl;
 }
 
+// print list
 void list_display(list *head){
     cout << "--- Displaying linked list ---" << endl;
-    // list *temporary = head;
+    int i = 0;
     while(head != NULL){
         cout << head->value << "\t" << head->index << endl;
         head = head->next;  
+        i++;
     }
+    cout << "List has " << i << " nodes..." << endl;
 }
 
 int main(){
     srand(time(NULL));
     list *head = NULL;
     list *temp;
-    // list *found;
-    
+
+    // adding nodes into list at the head of list
     for(int i = 0; i < 10; i ++){
         temp = add_to_list(rand()%10,rand()%10); //get pointer to a new node
         temp->next = head; // setting node's pointer to head(NULL)
         head = temp; //pointer of new node is now head pointer
     }
 
+
+    // manually adding nodes into list at the head of list
+    // head = insert_at_head(head,10,1);
+    // head = insert_at_head(head,11,2);
+    // head = insert_at_head(head,12,3);
+
+    cout << "------------------------------" << endl;
     list_display(head);
-    cout << endl;
-    // found = findNode(head,5);
-    // cout << "Node with index " << found->index << " found!" << endl;
-    // cout << "Value of the node with index " << found->index << " is " << found->value << endl; // replaced this block with the function
-    findNode(head,5);
+    cout << endl << "------------------------------" << endl;
+    
+    findNode(head,2);
+    cout << endl << "------------------------------" << endl;
 
-    insert_inbetween(15,5,6,head);
-    // list_display(head);
-
-    findEnd(head);
-
-    insert_at_the_end(20,10,head);
+    insert_inbetween(15,5,2,head);
+    cout << "------------------------------" << endl;
 
     list_display(head);
+    cout << "------------------------------" << endl;
+
+    list *tail = findEnd(head);
+
+    insert_at_the_end(23,16,tail);
+    cout << "------------------------------" << endl;
+
+    list_display(head);
+    cout << "------------------------------" << endl;
 
     destroy_list(head);
+    cout << "------------------------------" << endl;
  
     return 0;
 }
